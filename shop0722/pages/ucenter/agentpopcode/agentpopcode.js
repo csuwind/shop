e9b2ad874fd048b546471ea04661e99e7155d264
csu_wind <csu_wind@126.com>
2019-07-23 22:44:23 +0800

    0722前端

---
+++ shop0722/pages/ucenter/agentpopcode/agentpopcode.js
@@ -0,0 +1,76 @@
+// pages/ucenter/agentpopcode/agentpopcode.js
+var wxbarcode = require('../../../utils/index.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wxbarcode.qrcode('qrcode', '1234567890123456789', 420, 420);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '小程序商城',
+      path: '/pages/index/index'
+      // imageUrl:"" 
+    }
+  }
+})
